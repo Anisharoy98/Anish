aebbebf866dad3b4235b3e2ae82179600278b77d
Anisharoy98 <crazieeanish98@gmail.com>
2019-10-18 15:03:38 +0530

    conflicts

--- src/org/bank/AxisBank.java
+++ src/org/bank/AxisBank.java
@@ -2,12 +2,16 @@ package org.bank;
 
 public class AxisBank extends BankInfo {
 public void deposit() {
-	System.out.println("6% deposit");
+	System.out.println("6% deposit");}
+	public void add() {
+		System.out.println("0 %");
+	}
 	
-}
+
 public static void main(String[] args) {
 	AxisBank ax = new AxisBank();
 	ax.Bankinformation();
+	ax.add();
 	ax.sav();
 	ax.fix();
 	ax.deposit();
