aa7ac69541a7257e54dd333e8d97152495565690
Anisharoy98 <crazieeanish98@gmail.com>
2019-10-18 13:54:39 +0530

    Anisha First commit

---
+++ src/org/bank/AxisBank.java
@@ -0,0 +1,16 @@
+package org.bank;
+
+public class AxisBank extends BankInfo {
+public void deposit() {
+	System.out.println("6% deposit");
+	
+}
+public static void main(String[] args) {
+	AxisBank ax = new AxisBank();
+	ax.Bankinformation();
+	ax.savings();
+	ax.fixed();
+	ax.deposit();
+}
+
+}
---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,16 @@
+package org.bank;
+
+public class BankInfo {
+	public void Bankinformation() {
+		System.out.println("Bank Details");
+	}
+public void savings() {
+	System.out.println("2% savings");
+}
+public void fixed() {
+	System.out.println("3% fixed");
+}
+public void deposit() {
+	System.out.println("5% deposit");
+}
+}
